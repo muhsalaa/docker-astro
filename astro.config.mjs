267de22cc58f34f1673e294563bcce71fd4c2e4d
muhsalaa <muhsalaa@gmail.com>
2024-09-28 05:58:25 +0700

    chore: add docker dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,17 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  preview: {
+    port: 8080,
+    // exit if the port is already in use, instead of automatically trying the next available port.
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    // origin to serve assets from. before i run the server on 3000, so image not showing
+    // when i change port to serve to 8080 image showing
+    origin: "http://0.0.0.0:8080",
+  },
 });
